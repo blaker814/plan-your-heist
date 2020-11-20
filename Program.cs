01a53fabf3981af9cbe86ac6c5c537a4b6c9fe0e
blaker814 <rab0196@yahoo.com>
2020-11-20 11:44:32 -0600

    phase two complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanYourHeist
 {
@@ -7,54 +8,68 @@ namespace PlanYourHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
-            TeamMember heistMember = new TeamMember();
-            Console.Write("Enter a team member's name: ");
-            heistMember.Name = Console.ReadLine();
+            List<TeamMember> heistTeam = new List<TeamMember>();
             while (true)
             {
-                Console.Write("Enter their skill level: ");
-                try
+                TeamMember heistMember = new TeamMember();
+                Console.Write("Enter a team member's name: ");
+                string stopLoop = Console.ReadLine();
+                if (stopLoop == "")
                 {
-                    int skill = int.Parse(Console.ReadLine());
-                    if (skill > 0)
+                    break;
+                }
+                heistMember.Name = stopLoop;
+                while (true)
+                {
+                    Console.Write("Enter their skill level: ");
+                    try
                     {
-                        heistMember.SkillLevel = skill;
-                        break;
+                        int skill = int.Parse(Console.ReadLine());
+                        if (skill > 0)
+                        {
+                            heistMember.SkillLevel = skill;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a positive integer");
+                        }
                     }
-                    else
+                    catch
                     {
                         Console.WriteLine("Please enter a positive integer");
                     }
                 }
-                catch
+                while (true)
                 {
-                    Console.WriteLine("Please enter a positive integer");
-                }
-            }
-            while (true)
-            {
-                Console.Write("Enter their courage factor (0.0 - 2.0): ");
-                try
-                {
-                    double courage = double.Parse(Console.ReadLine());
-                    if (courage >= 0 && courage <= 2)
+                    Console.Write("Enter their courage factor (0.0 - 2.0): ");
+                    try
                     {
-                        heistMember.CourageFactor = courage;
-                        break;
+                        double courage = double.Parse(Console.ReadLine());
+                        if (courage >= 0 && courage <= 2)
+                        {
+                            heistMember.CourageFactor = courage;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a number between 0.0 and 2.0");
+                        }
                     }
-                    else
+                    catch
                     {
                         Console.WriteLine("Please enter a number between 0.0 and 2.0");
                     }
                 }
-                catch
-                {
-                    Console.WriteLine("Please enter a number between 0.0 and 2.0");
-                }
+                heistTeam.Add(heistMember);
             }
-            Console.WriteLine(@$"Name: {heistMember.Name}
-Skill level: {heistMember.SkillLevel}
-Courage factor: {heistMember.CourageFactor}");
+            Console.WriteLine($"Your heist team contains {heistTeam.Count} members");
+            foreach (TeamMember member in heistTeam)
+            {
+                Console.WriteLine(@$"Name: {member.Name}
+Skill level: {member.SkillLevel}
+Courage factor: {member.CourageFactor}");
+            }
         }
     }
 }
